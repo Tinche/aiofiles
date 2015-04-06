20ac17150027b8f83b5590a20b7003d97f4ba394
Ludovic Gasc (GMLudo) <git@gmludo.eu>
2015-04-06 23:35:41 +0200

    Add "Topic :: System :: Filesystems" as classifier in setup.py

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Topic :: System :: Filesystems",
     ],
     extras_require={
         ':python_version == "3.3"': ['asyncio', 'singledispatch']
