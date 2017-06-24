a31dc154b5701500f65489c928aa67b0644b118f
Chris Jerdonek <chris.jerdonek@gmail.com>
2017-06-24 10:00:23 -0700

    DRY up os.py.

--- aiofiles/os.py
+++ aiofiles/os.py
@@ -1,24 +1,22 @@
 """Async executor versions of file functions from the os module."""
 import asyncio
-import sys
 from functools import partial, wraps
 import os
 
 
-@asyncio.coroutine
-@wraps(os.stat)
-def stat(path, *, dir_fd=None, follow_symlinks=True, loop=None, executor=None):
-    if loop is None:
-        loop = asyncio.get_event_loop()
-    fun = partial(os.stat, path, dir_fd=dir_fd, follow_symlinks=follow_symlinks)
-    return loop.run_in_executor(executor, fun)
-
-
-if hasattr(os, "sendfile"):
+def wrap(func):
     @asyncio.coroutine
-    @wraps(os.sendfile)
-    def sendfile(out, in_fd, offset, nbytes, *, loop=None, executor=None):
+    @wraps(func)
+    def run(*args, loop=None, executor=None, **kwargs):
         if loop is None:
             loop = asyncio.get_event_loop()
-        fun = partial(os.sendfile, out, in_fd, offset, nbytes)
-        return loop.run_in_executor(executor, fun)
+        pfunc = partial(func, *args, **kwargs)
+        return loop.run_in_executor(executor, pfunc)
+
+    return run
+
+
+stat = wrap(os.stat)
+
+if hasattr(os, "sendfile"):
+    sendfile = wrap(os.sendfile)
