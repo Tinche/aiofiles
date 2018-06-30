13fad2c5c79a464b9fc040b2c27eb8a84803b527
Chris Seymour <christopher.seymour@nanoporetech.com>
2018-06-30 14:16:40 +0100

    can't support but py3.4 and py3.7

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
