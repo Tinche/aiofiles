ea485306be0a68c6841f456b4ae39ea624d36f63
alemigo <32462069+alemigo@users.noreply.github.com>
2018-10-25 17:11:24 +0100

    Add files via upload

---
+++ tempfile/temptypes.py
@@ -0,0 +1,63 @@
+"""Async wrappers for spooled temp files and temp directory objects"""
+
+# Imports
+import asyncio
+from ..base import AsyncBase
+from ..threadpool.utils import (delegate_to_executor, proxy_property_directly,
+                                cond_delegate_to_executor)
+from functools import partial
+
+
+@delegate_to_executor('fileno', 'rollover')
+@cond_delegate_to_executor('close', 'flush', 'isatty', 'newlines', 'read',
+                           'readline', 'readlines', 'seek', 'tell',
+                           'truncate')
+@proxy_property_directly('closed', 'encoding', 'mode', 'name', 'softspace')
+class AsyncSpooledTemporaryFile(AsyncBase):
+    """Async wrapper for SpooledTemporaryFile class"""
+
+    @asyncio.coroutine
+    def _check(self):
+        if self._file._rolled: return
+        max_size = self._file._max_size
+        if max_size and self._file.tell() > max_size:
+            yield from self.rollover()
+
+    @asyncio.coroutine
+    def write(self, s):
+        """Implementation to anticipate rollover"""
+        if self._file._rolled:
+            cb = partial(self._file.write, s)
+            return (yield from self._loop.run_in_executor(self._executor, cb))
+        else:
+            file = self._file._file #reference underlying base IO object
+            rv = file.write(s)
+            yield from self._check()
+            return rv
+
+    @asyncio.coroutine
+    def writelines(self, iterable):
+        """Implementation to anticipate rollover"""
+        if self._file._rolled:
+            cb = partial(self._file.writelines, iterable)
+            return (yield from self._loop.run_in_executor(self._executor, cb))
+        else:
+            file = self._file._file #reference underlying base IO object
+            rv = file.writelines(iterable)
+            yield from self._check()
+            return rv
+
+
+@delegate_to_executor('cleanup')
+@proxy_property_directly('name')
+class AsyncTemporaryDirectory:
+    """Async wrapper for TemporaryDirectory class"""
+
+    def __init__(self, file, loop, executor):
+        self._file = file
+        self._loop = loop
+        self._executor = executor
+
+    @asyncio.coroutine
+    def close(self):
+        yield from self.cleanup()
