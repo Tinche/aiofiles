876c1cebcc3c0006e60fb6c66ef23b966f011fbd
Thomas Grainger <tagrain@gmail.com>
2019-01-24 10:23:29 +0000

    show the wrapped file in AsyncBase.__repr__

--- aiofiles/base.py
+++ aiofiles/base.py
@@ -12,6 +12,9 @@ class AsyncBase:
     def __aiter__(self):
         """We are our own iterator."""
         return self
+    
+    def __repr__(self):
+        return super().__repr__() + ' wrapping ' + repr(self._file)
 
     @asyncio.coroutine
     def __anext__(self):
