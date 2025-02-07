bf6dcd0883714fd4883c515f5754a6f37a5edf1b
stankudrow <stankudrow@reply.no>
2025-02-07 11:51:24 +0300

    groom threadpool/test_wrap.py

--- tests/threadpool/test_wrap.py
+++ tests/threadpool/test_wrap.py
@@ -1,10 +1,12 @@
-from aiofiles.threadpool import wrap
+from io import FileIO
 
 import pytest
 
+from aiofiles.threadpool import wrap
 
-@pytest.mark.parametrize("entity", [int, [1, 2, 3], lambda x: x**x])
-def test_threadpool_wrapper(entity):
+
+@pytest.mark.parametrize("entity", [int, [1, 2, 3], lambda x: x**x, FileIO])
+def test_threadpool_wrapper_negative(entity):
     """Raising TypeError when wrapping unsupported entities."""
 
     with pytest.raises(TypeError):
